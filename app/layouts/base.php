bc53626409cadf4a27b9cb0f14e495937d38098f
Garet C. Green <17110935+nox7@users.noreply.github.com>
2021-06-21 14:07:33 -0500

    Create base documentation on Routing

--- app/layouts/base.php
+++ app/layouts/base.php
@@ -4,9 +4,14 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="/css/theme/main.css">
+		<link rel="stylesheet" href="/external/prism/prism.css">
+		<script defer src="/external/prism/prism.js"></script>
 		<?= $htmlHead ?>
 	</head>
 	<body>
-		<?= $htmlBody ?>
+		<page-container>
+			<?php include __DIR__ . "/../views/_partials/_sidebar.html"; ?>
+			<?= $htmlBody ?>
+		</page-container>
 	</body>
 </html>
