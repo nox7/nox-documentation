22ceec6ef1f6799456db3347f2e26f9bce549221
Garet C. Green <17110935+nox7@users.noreply.github.com>
2021-06-21 13:00:54 -0500

    Update to new Nox

--- app/nox-request.php
+++ app/nox-request.php
@@ -1,15 +1,21 @@
 <?php
 	require_once __DIR__ . "/../vendor/autoload.php";
 
+	$requestPath = $_GET['requestPath'];
+	$requestMethod = $_SERVER['REQUEST_METHOD'];
+
 	// Load the router
-	$router = new Nox\Router\Router;
+	$router = new Nox\Router\Router(
+		requestPath: $requestPath,
+		requestMethod: $requestMethod,
+	);
 	$router->loadAll(__DIR__);
 
 	// Load the request handler
 	$requestHandler = new \Nox\Router\RequestHandler(
 		$router,
-		$_GET['requestPath'],
-		$_SERVER['REQUEST_METHOD']
+		$requestPath,
+		$requestMethod,
 	);
 
 	// Process the request
