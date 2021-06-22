dff8c9bcd61ad5b358e68bfd161b797417aac5df
Garet C. Green <17110935+nox7@users.noreply.github.com>
2021-06-22 10:07:16 -0500

    Docs for layouts, views, custom attributes

---
+++ app/controllers/v1/V1PagesController.php
@@ -0,0 +1,24 @@
+<?php
+	use Nox\RenderEngine\Renderer;
+	use Nox\Router\Attributes\Route;
+	use Nox\Router\Attributes\RouteBase;
+	use Nox\Router\BaseController;
+
+	#[RouteBase("/docs/1.x")]
+	class V1PagesController extends BaseController{
+
+		#[Route("GET", "/routing")]
+		public function routeView(): string{
+			return Renderer::renderView("docs/v1/routing.html");
+		}
+
+		#[Route("GET", "/routing/custom-route-method-attribute")]
+		public function routeMethodAttributeView(): string{
+			return Renderer::renderView("docs/v1/custom-method-attributes.html");
+		}
+
+		#[Route("GET", "/layouts")]
+		public function layoutsView(): string{
+			return Renderer::renderView("docs/v1/layouts.html");
+		}
+	}
