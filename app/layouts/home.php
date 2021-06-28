98326355f7561bb1c5b5f21ad3d56e111c01b8ba
nox7 <17110935+nox7@users.noreply.github.com>
2021-06-28 16:32:18 -0500

    Create dedicated, sidebar-less homepage layout

---
+++ app/layouts/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="/css/theme/main.css">
+		<link rel="stylesheet" href="/external/prism/prism.css">
+		<script defer src="/external/prism/prism.js"></script>
+		<?= $htmlHead ?>
+	</head>
+	<body>
+		<page-container>
+			<?= $htmlBody ?>
+		</page-container>
+	</body>
+</html>
