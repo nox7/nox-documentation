649b5dd8e9793f80883e5bcabccd4898b58571c5
Garet C. Green <17110935+nox7@users.noreply.github.com>
2021-07-21 12:21:51 -0500

    Bring nox-request.php format to new format

--- app/nox-request.php
+++ app/nox-request.php
@@ -1,22 +1,27 @@
 <?php
+
+	use Nox\ORM\Abyss;
+
 	require_once __DIR__ . "/../vendor/autoload.php";
 
-	$requestPath = parse_url($_SERVER['REQUEST_URI'])['path'];
+	$requestPath = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 	$requestMethod = $_SERVER['REQUEST_METHOD'];
 
 	// Load the router
 	$router = new Nox\Router\Router(
-		requestPath: $requestPath,
-		requestMethod: $requestMethod,
+		requestPath:$requestPath,
+		requestMethod:$requestMethod,
 	);
-	$router->loadAll(__DIR__);
+	$router->loadAll(
+		fromDirectory: __DIR__,
+	);
+
+	// Load the Abyss ORM configuration
+	Abyss::loadConfig(__DIR__);
 
 	// Load the request handler
-	$requestHandler = new \Nox\Router\RequestHandler(
-		$router,
-		$requestPath,
-		$requestMethod,
-	);
+	$requestHandler = new \Nox\Router\RequestHandler($router);
+	\Nox\Router\BaseController::$requestHandler = $requestHandler;
 
 	// Process the request
 	$requestHandler->processRequest();
