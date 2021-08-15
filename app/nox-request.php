f28c14481f239f5d4298993aa2dfa608629466df
Cole Green <17110935+nox7@users.noreply.github.com>
2021-08-15 10:42:55 -0500

    Register pages in dynamic route

--- app/nox-request.php
+++ app/nox-request.php
@@ -4,6 +4,7 @@
 
 	require_once __DIR__ . "/../vendor/autoload.php";
 	require_once __DIR__ . "/nox-env.php";
+	require_once __DIR__ . "/classes/Page.php";
 
 	$requestPath = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 	$requestMethod = $_SERVER['REQUEST_METHOD'];
@@ -23,6 +24,26 @@
 	// Load the request handler
 	$requestHandler = new \Nox\Router\RequestHandler($router);
 	\Nox\Router\BaseController::$requestHandler = $requestHandler;
+
+	// Load all pages into the router
+	$abyss = new Abyss();
+	$pages = $abyss->fetchInstances(
+		model:Page::getModel(),
+	);
+	/** @var Page $page */
+	foreach($pages as $page){
+		$fullRoute = $page->getFullRoute();
+		$router->addDynamicRoute(
+			new \Nox\Router\DynamicRoute(
+				requestMethod: "get",
+				requestPath: $fullRoute,
+				isRegex:false,
+				onRender: Closure::fromCallable(function() use ($page){
+					return $page->getRenderedPage();
+				}),
+			),
+		);
+	}
 
 	// Process the request
 	$requestHandler->processRequest();
