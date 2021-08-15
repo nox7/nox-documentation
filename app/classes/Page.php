b03bcc1c417989b2c96217ca205e9dde2485d548
Cole Green <17110935+nox7@users.noreply.github.com>
2021-08-15 10:41:46 -0500

    Add method to fetch the full route of a page
    Based on its parent categories

--- app/classes/Page.php
+++ app/classes/Page.php
@@ -24,4 +24,35 @@
 		public function __construct(){
 			parent::__construct($this);
 		}
+
+		/**
+		 * Traverses up the parent categoryIDs until it hits
+		 * a page category with a null parent category ID. Then builds
+		 * the route
+		 */
+		public function getFullRoute(): string{
+			// These routes are in the order of the child to parent.
+			// So it will need to be reversed before constructing the route base
+			$parentRoutes = [];
+			if ($this->categoryID !== null) {
+				$abyss = new Abyss();
+				$parentCategoryID = $this->categoryID;
+				do {
+					/** @var PageCategory $pageCategory */
+					$pageCategory = $abyss->fetchInstanceByModelPrimaryKey(
+						model:PageCategory::getModel(),
+						keyValue: $parentCategoryID,
+					);
+					if ($pageCategory !== null){
+						$parentRoutes[] = $pageCategory->routeBase;
+						$parentCategoryID = $pageCategory->parentCategoryID;
+					}else{
+						$parentCategoryID = null;
+					}
+				}while ($parentCategoryID !== null);
+			}
+
+			$reversedParentRoutes = array_reverse($parentRoutes);
+			return sprintf("%s%s", implode("", $reversedParentRoutes), $this->route);
+		}
 	}
