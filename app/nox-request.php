5d3d9d93cf0a27e024bc1e0ff5b573bce34cfdc9
Cole Green <17110935+nox7@users.noreply.github.com>
2021-08-17 17:51:10 -0500

    Remove MySQL features

--- app/nox-request.php
+++ app/nox-request.php
@@ -4,7 +4,6 @@
 
 	require_once __DIR__ . "/../vendor/autoload.php";
 	require_once __DIR__ . "/nox-env.php";
-	require_once __DIR__ . "/classes/Page.php";
 
 	$requestPath = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 	$requestMethod = $_SERVER['REQUEST_METHOD'];
@@ -24,26 +23,6 @@
 	// Load the request handler
 	$requestHandler = new \Nox\Router\RequestHandler($router);
 	\Nox\Router\BaseController::$requestHandler = $requestHandler;
-
-	// Load all pages into the router
-	$abyss = new Abyss();
-	$pages = $abyss->fetchInstances(
-		model:Page::getModel(),
-	);
-	/** @var Page $page */
-	foreach($pages as $page){
-		$fullRoute = $page->getFullRoute();
-		$router->addDynamicRoute(
-			new \Nox\Router\DynamicRoute(
-				requestMethod: "get",
-				requestPath: $fullRoute,
-				isRegex:false,
-				onRender: Closure::fromCallable(function() use ($page){
-					return $page->getRenderedPage();
-				}),
-			),
-		);
-	}
 
 	// Process the request
 	$requestHandler->processRequest();
