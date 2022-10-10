fd0ab56c791e7c95ee43a19c80891df63ef9beeb
Garet C. Green <17110935+nox7@users.noreply.github.com>
2022-10-10 08:54:06 -0500

    Create SearchDocsTest.php

---
+++ tests/SearchDocsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+	require_once __DIR__ . "/../app/nox-env.php";
+	require_once __DIR__ . "/../vendor/autoload.php";
+
+	use NoxDocumentation\Docs\SearchService;
+	use PHPUnit\Framework\TestCase;
+
+	final class SearchDocsTest extends TestCase
+	{
+
+		public static function setUpBeforeClass(): void{
+			$nox = new Nox\Nox();
+			$nox->setSourceCodeDirectory(__DIR__ . "/../app/src");
+		}
+
+		public function testSearchDocsV2(): void
+		{
+			// Currently, there are only two documents that reference $viewScope. If there are ever more, this
+			// test needs to be updated.
+			$reflectionMethods = SearchService::query("2.0", "\$viewScope");
+
+			$this->assertNotEmpty($reflectionMethods);
+			$this->assertCount(2, $reflectionMethods);
+		}
+	}
