08b696c7eda85bef1d955486c450100432bc7f9a
Garet C. Green <17110935+nox7@users.noreply.github.com>
2022-10-10 14:04:06 -0500

    There is no editor

--- app/resources/static/js/editor/main.js
+++
@@ -1,9 +0,0 @@
-import EditorWrapper from "./EditorWrapper.js";
-import EditTabButtons from "./EditTabButtons.js";
-import EditorForm from "./EditorForm.js";
-
-const aceEditor = ace.edit("page-content")
-const editorWrapper = new EditorWrapper(aceEditor);
-
-editorWrapper.sessions.body.setValue(document.querySelector("#body-preload").value);
-editorWrapper.sessions.head.setValue(document.querySelector("#head-preload").value);
